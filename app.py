dd3b0c02771747d2113c9e32ae559c959380abd9
Jake Etzler <jae016@bucknell.edu>
2022-03-28 03:52:22 -0400

    Update: Backend task prioritizing algorithm and frontend table implementation

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ tm = tasks.TasksModel()
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', all_table=tm.getAllTable(), due_table=tm.getDueTable())
+    return render_template('index.html', all_table=tm.getAllTable(), due_table=tm.getDueTable(),
+                           pri_table=tm.getPriTable())
 
 
 @app.route('/completed', methods=['GET', 'POST'])
--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
 from abc import ABC
+from audioop import reverse
 
 import checkvist
 from flask_table import Table, Col
+import datetime
 
 USER = 'jboss224@gmail.com'
-SECRET = '9SXUhi42q1EzyI'
+SECRET = 'UV9ipJFULQQa6v'
 
 
 #
@@ -19,12 +21,33 @@ class TaskTable(Table):
 
 class Task:
     def __init__(self, task):
-        self.name = str(task['content'][3:])
+        self.task = task
+        self.name = str(task['content'][3:]) + str(task['tags_as_text'])
         self.due = str(task['due'])
         self.tdict = dict(name=self.name, due=self.due)
         self.status = str(task['status'])
+        self.tags = task['tags_as_text']
+        self.duedate = self.getDueDate()
+        self.time = self.getTime()
+
+
+    # Get date from tags
+    def getDueDate(self):
+        date = datetime.date(int(self.task['due'][:4]), int(self.task['due'][5:7]), int(self.task['due'][8:]))
+        return date
+
+    # Get time from tags
+    def getTime(self):
+        for i in range(len(self.tags)):
+            if self.tags[i] == 't':
+                return int(self.tags[i + 1])
+        return 0
+
+
+
 
 class TasksModel:
+    # Initialize the model
     def __init__(self):
         self.cl = getclient()
         self.tasks = self.getTasks()
@@ -55,10 +78,40 @@ class TasksModel:
         table = TaskTable(items)
         return table
 
+    # Get list of due dates as Date comparable object
+    # TODO: Obsolete with updates Task class attributes
+    def getDueDates(self):
+        dates = []
+        for t in self.tasks:
+            date = datetime.date(int(t['due'][:4]), int(t['due'][5:7]), int(t['due'][8:]))
+            dates.append((t['content'][3:], date))
+        return dates
+
     # Get due tasks
     def getDueTable(self):
         items = []
         for t in self.tasks:
+            temp = Task(t)
+            if temp.status == '0':
+                items.append(temp.tdict)
+        table = TaskTable(items)
+        return table
+
+    # Get list of tasks ordered by priority
+    def getPriTasks(self):
+        items = []
+        for t in self.tasks:
+            items.append(Task(t))
+        pri_items = sorted(sorted(items, key=lambda x: x.time, reverse=True), key=lambda x: x.duedate)
+        pri_list = []
+        for t in pri_items:
+            pri_list.append(t.task)
+        return pri_list
+
+    # Get prioritized task table\
+    def getPriTable(self):
+        items = []
+        for t in self.getPriTasks():
             temp = Task(t)
             if temp.status == '0':
                 items.append(temp.tdict)
@@ -76,6 +129,18 @@ def getclient():
     user = cl.get_user()
     print(user['username'])
     return cl
+
+
+
+
+if __name__ == '__main__':
+    tm = TasksModel()
+    tasks = tm.getTasks()
+    for t in tm.getPriTasks():
+        print(t['content'])
+        print(t['due'])
+        print(t['tags_as_text'])
+
 
 
 
