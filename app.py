17a25a14cc28c1eb3ce7574098e356ebbb74f9b2
Jake Etzler <jae016@bucknell.edu>
2022-03-30 17:46:06 -0400

    Cleanup: methods added to index app route

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 tm = tasks.TasksModel()
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return render_template('index.html', all_table=tm.getAllTable(), due_table=tm.getDueTable(),
                            pri_table=tm.getPriTable(), cal=tasks.getcalendar())
@@ -17,10 +18,10 @@ def hello_world():
 def completed():
     return render_template('completed.html', comp_table=tm.getCompTable())
 
+
 @app.route('/due', methods=['GET', 'POST'])
 def due():
     return render_template('due.html', due_table=tm.getDueTable())
-
 
 
 
